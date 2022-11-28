1ca57a684801c1a794f0a3dc70253cbb61dd12ec
Cory Walker <cyb12345@comcast.net>
2022-11-28 17:58:10 -0500

    1.3.5. done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-//import React from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
 import React, { useState } from 'react';
 
-//const [currentCategory, setCurrentCategory] = useState(categories[0]);
 function App() { 
   const [categories] = useState([
     {
@@ -29,7 +27,7 @@ function App() {
       currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+      <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
