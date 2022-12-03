6db02754265f9391731b2b86764abe14a722f1a1
Cory Walker <cyb12345@comcast.net>
2022-12-03 10:59:25 -0500

    error handling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {validateEmail} from '../../utils/helpers';
 
 function ContactForm() {
 
@@ -11,12 +12,31 @@ function ContactForm() {
     const {name, email, message} = formState;
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid');
+            } 
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value })
+        }
+        
+        console.log('errorMessage', errorMessage);
     }
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
     }
+
+    const [errorMessage, setErrorMessage] = useState('');
 
  return (
      <section>
