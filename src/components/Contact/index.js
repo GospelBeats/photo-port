9b4f02ddb7be39321bd275065b0a0ae623f0ad33
Cory Walker <cyb12345@comcast.net>
2022-12-05 18:00:50 -0500

    contact nav updated

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,3 +1,4 @@
+// start 1.4.6
 import React, {useState} from 'react';
 import {validateEmail} from '../../utils/helpers';
 
@@ -14,7 +15,7 @@ function ContactForm() {
     function handleChange(e) {
         if (e.target.name === 'email') {
             const isValid = validateEmail(e.target.value);
-            console.log(isValid);
+            // console.log(isValid);
             if (!isValid) {
                 setErrorMessage('Your email is invalid');
             } 
@@ -28,14 +29,12 @@ function ContactForm() {
         if (!errorMessage) {
             setFormState({...formState, [e.target.name]: e.target.value })
         }
-        
-        console.log('errorMessage', errorMessage);
+        // console.log('errorMessage', errorMessage);
     }
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
     }
-
     const [errorMessage, setErrorMessage] = useState('');
 
  return (
@@ -44,16 +43,21 @@ function ContactForm() {
          <form id="contact-form" onSubmit={handleSubmit}>
          <div>
             <label htmlFor="name">Name:</label>
-            <input type="text" name="name" defaultValue={name} onChange={handleChange}/>
+            <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
             </div>
             <div>
             <label htmlFor="email">Email address:</label>
-            <input type="email" name="email" onChange={handleChange} defaultValue={email}/>
+            <input type="email" name="email" onBlur={handleChange} defaultValue={email}/>
             </div>
             <div>
             <label htmlFor="message">Message:</label>
-            <textarea name="message" rows="5" onChange={handleChange} defaultValue={message} />
+            <textarea name="message" rows="5" onBlur={handleChange} defaultValue={message} />
             </div>
+            {errorMessage && (
+                <div>
+                    <p className="error-text">{errorMessage}</p>
+                </div>
+            )}
             <button type="submit">Submit</button>
          </form>
      </section>
